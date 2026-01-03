f8a2e1d3d3d6e616de51bc4c3a6468024d6d9cdf
goodbyebada <goodbyebada@naver.com>
2026-01-03 19:59:31 +0900

    feat: / 으로 연결

--- app/index.tsx
+++ app/index.tsx
@@ -11,7 +11,8 @@ export default function Index() {
 
   const domain = config.domain;
   // 할일 추가 페이지 test 위한 path
-  const DEFAULT_PATH = '/task/create';
+  // const DEFAULT_PATH = '/task/create';
+  const DEFAULT_PATH = '/';
   const webUrl = `${domain}${DEFAULT_PATH}`;
 
   return (
