59b6d9cf2e2cb8f3123293f3a33a12af158f1e52
dexical30 <dexical30@gmail.com>
2026-02-27 23:28:38 +0900

    fixed: TODO: access token이 reissue를 통해 발급되지 않는것으로 확인됨

--- src/hooks/useSyncLoginCookie.ts
+++ src/hooks/useSyncLoginCookie.ts
@@ -2,8 +2,9 @@ import { useCallback } from 'react';
 import NitroCookies from 'react-native-nitro-cookies';
 import useAuthStore from '../stores/useAuthStore';
 import { config } from '../utils/envConfig';
-import axios, { Axios } from 'axios';
+import axios, { Axios, AxiosResponse } from 'axios';
 import { issueAccessToken } from '../api/generated/endpoints/auth/auth';
+import { ApiResponseString } from '../api/generated/models';
 
 const useSyncLoginCookie = () => {
   const { setAccessToken, setRefreshToken } = useAuthStore();
@@ -17,15 +18,29 @@ const useSyncLoginCookie = () => {
         setRefreshToken(refreshToken);
       }
 
-      const { data: requestAccessTokenResponse } = await issueAccessToken();
+      const response = await fetch(`${config.domain}/v1/auth/reissue`, {
+        method: 'POST',
+        headers: {
+          Cookie: `refresh=${refreshToken}`,
+        },
+      });
 
-      const accessToken = await requestAccessTokenResponse.text();
-      console.log('accessToken', accessToken);
-      if (accessToken) {
-        setAccessToken(accessToken);
-      }
+      // const reissueResponseJson = await response.json();
+      const reissueResponseText = await response.text();
+
+      // TODO: access token 이 제대로 적용되지 않는것으로 보임
+      console.log(
+        'reissueResponseText',
+        response,
+        'reissueResponseText',
+        reissueResponseText,
+      );
+      // const accessToken = requestAccessTokenResponse?.code;
+      // if (accessToken) {
+      //   setAccessToken(accessToken);
+      // }
     }
-  }, [setAccessToken, setRefreshToken]);
+  }, []);
 };
 
 export default useSyncLoginCookie;
