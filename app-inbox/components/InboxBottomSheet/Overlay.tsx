fe68c5baf9156ef80a8f99f73be2fb7766b6e9de
dexical30 <dexical30@gmail.com>
2026-03-01 17:26:15 +0900

    refactor: overlay 에 onClose 이벤트전달

--- app-inbox/components/InboxBottomSheet/Overlay.tsx
+++ app-inbox/components/InboxBottomSheet/Overlay.tsx
@@ -1,7 +1,29 @@
-import { View } from 'react-native';
+import { Pressable, View } from 'react-native';
+import Animated, {
+  useSharedValue,
+  withDelay,
+  withSpring,
+  withTiming,
+} from 'react-native-reanimated';
+import { useInboxBottomSheet } from './context';
+import { useEffect } from 'react';
 
 const Overlay = () => {
-  return <View className="absolute inset-0 bg-black/50" />;
+  const { onClose } = useInboxBottomSheet();
+  const opacity = useSharedValue(0);
+
+  useEffect(() => {
+    opacity.value = withDelay(100, withSpring(0.5, { duration: 300 }));
+  }, []);
+
+  return (
+    <Pressable onPress={onClose} className="absolute inset-0">
+      <Animated.View
+        className="absolute inset-0 bg-black"
+        style={{ opacity }}
+      />
+    </Pressable>
+  );
 };
 
 export default Overlay;
