f7aca67aa9edf38313f758ac45b8d41b84adf8b3
dexical30 <dexical30@gmail.com>
2026-03-06 14:50:49 +0900

    feature: animaion disabled

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout() {
           <Stack
             screenOptions={{
               headerShown: false,
+              animation: 'none',
             }}
           />
         </SafeAreaProvider>
