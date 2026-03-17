41f9c9da21bd0b93ca9f0683cfad3407ed37e6d3
dexical30 <dexical30@gmail.com>
2026-03-17 21:55:18 +0900

    fixed: 포탈 context 추가

--- src/components/Portal/index.tsx
+++ src/components/Portal/index.tsx
@@ -20,6 +20,14 @@ const PortalContext = createContext<PortalContextType>({
   unregister: () => {},
 });
 
+export const usePortalContext = () => {
+  const context = useContext(PortalContext);
+  if (!context) {
+    throw new Error('usePortalContext must be used within a PortalProvider');
+  }
+  return context;
+};
+
 export const PortalProvider = ({
   children,
   rootKey,
